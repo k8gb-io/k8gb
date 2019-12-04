de9e7de5a6a2fdeb827f730c36d8fcceb5a5ccfe
Yury Tsarev <yury.tsarev@absa.africa>
2019-12-04 12:14:31 +0100

    First iteration of ohmyglb operator
    Implements simple GslbResolver CRD/Controller which drives
    underlying coredns Deployment

---
+++ pkg/apis/ohmyglb/v1beta1/doc.go
@@ -0,0 +1,4 @@
+// Package v1beta1 contains API Schema definitions for the ohmyglb v1beta1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=ohmyglb.absa.oss
+package v1beta1
---
+++ pkg/controller/gslbresolver/gslbresolver_controller.go
@@ -0,0 +1,217 @@
+package gslbresolver
+
+import (
+	"context"
+	"reflect"
+
+	ohmyglbv1beta1 "github.com/AbsaOSS/ohmyglb/pkg/apis/ohmyglb/v1beta1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_gslbresolver")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new GslbResolver Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileGslbResolver{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("gslbresolver-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource GslbResolver
+	err = c.Watch(&source.Kind{Type: &ohmyglbv1beta1.GslbResolver{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create that are owned by the primary resource
+	// Watch for changes to secondary resource Pods and requeue the owner GslbResolver
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &ohmyglbv1beta1.GslbResolver{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileGslbResolver implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileGslbResolver{}
+
+// ReconcileGslbResolver reconciles a GslbResolver object
+type ReconcileGslbResolver struct {
+	// TODO: Clarify the split client
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a GslbResolver object and makes changes based on the state read
+// and what is in the GslbResolver.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
+// a GslbResolver Deployment for each GslbResolver CR
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileGslbResolver) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling GslbResolver")
+
+	// Fetch the GslbResolver instance
+	gslbresolver := &ohmyglbv1beta1.GslbResolver{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, gslbresolver)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			reqLogger.Info("GslbResolver resource not found. Ignoring since object must be deleted")
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		reqLogger.Error(err, "Failed to get GslbResolver")
+		return reconcile.Result{}, err
+	}
+
+	// Check if the deployment already exists, if not create a new one
+	found := &appsv1.Deployment{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: gslbresolver.Name, Namespace: gslbresolver.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.deploymentForGslbResolver(gslbresolver)
+		reqLogger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
+		if err != nil {
+			reqLogger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return reconcile.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment")
+		return reconcile.Result{}, err
+	}
+
+	// Ensure the deployment size is the same as the spec
+	size := gslbresolver.Spec.Size
+	if *found.Spec.Replicas != size {
+		found.Spec.Replicas = &size
+		err = r.client.Update(context.TODO(), found)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			return reconcile.Result{}, err
+		}
+		// Spec updated - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	// Update the GslbResolver status with the pod names
+	// List the pods for this gslbresolver's deployment
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(gslbresolver.Namespace),
+		client.MatchingLabels(labelsForGslbResolver(gslbresolver.Name)),
+	}
+	if err = r.client.List(context.TODO(), podList, listOpts...); err != nil {
+		reqLogger.Error(err, "Failed to list pods", "GslbResolver.Namespace", gslbresolver.Namespace, "GslbResolver.Name", gslbresolver.Name)
+		return reconcile.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, gslbresolver.Status.PodNames) {
+		gslbresolver.Status.PodNames = podNames
+		err := r.client.Status().Update(context.TODO(), gslbresolver)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update GslbResolver status")
+			return reconcile.Result{}, err
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// deploymentForGslbResolver returns a gslbresolver Deployment object
+func (r *ReconcileGslbResolver) deploymentForGslbResolver(m *ohmyglbv1beta1.GslbResolver) *appsv1.Deployment {
+	ls := labelsForGslbResolver(m.Name)
+	replicas := m.Spec.Size
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: "coredns/coredns:1.6.5",
+						Name:  "coredns",
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 53,
+							Name:          "coredns",
+						}},
+					}},
+				},
+			},
+		},
+	}
+	// Set GslbResolver instance as the owner and controller
+	controllerutil.SetControllerReference(m, dep, r.scheme)
+	return dep
+}
+
+// labelsForGslbResolver returns the labels for selecting the resources
+// belonging to the given gslbresolver CR name.
+func labelsForGslbResolver(name string) map[string]string {
+	return map[string]string{"app": "gslbresolver", "gslbresolver_cr": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
+}
