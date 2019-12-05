1771adf8731fab7b827aa0bd1cbfc78d9547fb6b
Yury Tsarev <yury.tsarev@absa.africa>
2019-12-05 17:00:16 +0100

    Gslb api and controller
    First iteration - Gslb instantiates GslbResolver

---
+++ pkg/apis/ohmyglb/v1beta1/gslb_types.go
@@ -0,0 +1,53 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// GslbSpec defines the desired state of Gslb
+// +k8s:openapi-gen=true
+type GslbSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// GslbStatus defines the observed state of Gslb
+// +k8s:openapi-gen=true
+type GslbStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// +listType=set
+	Hosts []string `json:"hosts"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Gslb is the Schema for the gslbs API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=gslbs,scope=Namespaced
+type Gslb struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GslbSpec   `json:"spec,omitempty"`
+	Status GslbStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// GslbList contains a list of Gslb
+type GslbList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Gslb `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Gslb{}, &GslbList{})
+}
--- pkg/apis/ohmyglb/v1beta1/zz_generated.openapi.go
+++ pkg/apis/ohmyglb/v1beta1/zz_generated.openapi.go
@@ -11,9 +11,56 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
+		"./pkg/apis/ohmyglb/v1beta1.Gslb":               schema_pkg_apis_ohmyglb_v1beta1_Gslb(ref),
 		"./pkg/apis/ohmyglb/v1beta1.GslbResolver":       schema_pkg_apis_ohmyglb_v1beta1_GslbResolver(ref),
 		"./pkg/apis/ohmyglb/v1beta1.GslbResolverSpec":   schema_pkg_apis_ohmyglb_v1beta1_GslbResolverSpec(ref),
 		"./pkg/apis/ohmyglb/v1beta1.GslbResolverStatus": schema_pkg_apis_ohmyglb_v1beta1_GslbResolverStatus(ref),
+		"./pkg/apis/ohmyglb/v1beta1.GslbSpec":           schema_pkg_apis_ohmyglb_v1beta1_GslbSpec(ref),
+		"./pkg/apis/ohmyglb/v1beta1.GslbStatus":         schema_pkg_apis_ohmyglb_v1beta1_GslbStatus(ref),
+	}
+}
+
+func schema_pkg_apis_ohmyglb_v1beta1_Gslb(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "Gslb is the Schema for the gslbs API",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("./pkg/apis/ohmyglb/v1beta1.GslbSpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("./pkg/apis/ohmyglb/v1beta1.GslbStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"./pkg/apis/ohmyglb/v1beta1.GslbSpec", "./pkg/apis/ohmyglb/v1beta1.GslbStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
 	}
 }
 
@@ -110,6 +157,50 @@ func schema_pkg_apis_ohmyglb_v1beta1_GslbResolverStatus(ref common.ReferenceCall
 					},
 				},
 				Required: []string{"podNames"},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_ohmyglb_v1beta1_GslbSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GslbSpec defines the desired state of Gslb",
+				Type:        []string{"object"},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_ohmyglb_v1beta1_GslbStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GslbStatus defines the observed state of Gslb",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"hosts": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "set",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "INSERT ADDITIONAL STATUS FIELD - define observed state of cluster Important: Run \"operator-sdk generate k8s\" to regenerate code after modifying this file Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+				},
+				Required: []string{"hosts"},
 			},
 		},
 	}
