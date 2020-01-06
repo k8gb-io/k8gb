613627622b3ff2e67a6a006e893ed79249b53d8f
Yury Tsarev <yury.tsarev@absa.africa>
2020-01-06 14:15:47 +0100

    Skip e2e test evaluation by `go test` in Github Actions CI

--- pkg/test/main_test.go
+++ pkg/test/main_test.go
@@ -1,11 +1,15 @@
 package e2e
 
 import (
+	"os"
 	"testing"
 
 	f "github.com/operator-framework/operator-sdk/pkg/test"
 )
 
 func TestMain(m *testing.M) {
+	if os.Getenv("GITHUB_WORKFLOW") != "" {
+		return
+	}
 	f.MainEntry(m)
 }
