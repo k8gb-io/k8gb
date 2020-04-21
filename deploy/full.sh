bd2d788bd30147f7745157aa2a321fc47ab45c3c
Yury Tsarev <yury.tsarev@absa.africa>
2020-04-21 08:51:36 +0200

    Override node role for single node cluster

--- deploy/full.sh
+++ deploy/full.sh
@@ -4,10 +4,11 @@
 # will never be executed as they contain the same underlying target as `deploy-first-ohmyglb`
 # but with different variables
 
+NODE_ROLE=${NODE_ROLE:-worker}
 make use-second-context
-export HOST_ALIAS_IP1=$(kubectl get nodes test-gslb2-worker -o custom-columns='IP:status.addresses[0].address' --no-headers)
+export HOST_ALIAS_IP1=$(kubectl get nodes test-gslb2-${NODE_ROLE} -o custom-columns='IP:status.addresses[0].address' --no-headers)
 make use-first-context deploy-first-ohmyglb deploy-gslb-cr $1
-export HOST_ALIAS_IP2=$(kubectl get nodes test-gslb1-worker -o custom-columns='IP:status.addresses[0].address' --no-headers)
+export HOST_ALIAS_IP2=$(kubectl get nodes test-gslb1-${NODE_ROLE} -o custom-columns='IP:status.addresses[0].address' --no-headers)
 make use-second-context deploy-second-ohmyglb deploy-gslb-cr $1
 
 make wait-for-nginx-ingress-ready
