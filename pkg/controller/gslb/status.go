e6ac68e052f15ecf26450532b0ea7c6b46ffd45f
Yury Tsarev <yury.tsarev@absa.africa>
2020-05-14 21:13:38 +0200

    Streamline Gslb Status
    * Remove redundant `ManagedHosts`
    * Show external IP addresses in `HealthyRecords` even if local service
      is `Unhealthy`
    * Expose `GeoTag`
    * Resolves #116

--- pkg/controller/gslb/status.go
+++ pkg/controller/gslb/status.go
@@ -2,6 +2,8 @@ package gslb
 
 import (
 	"context"
+	"os"
+	"regexp"
 
 	ohmyglbv1beta1 "github.com/AbsaOSS/ohmyglb/pkg/apis/ohmyglb/v1beta1"
 	externaldns "github.com/kubernetes-incubator/external-dns/endpoint"
@@ -12,8 +14,6 @@ import (
 )
 
 func (r *ReconcileGslb) updateGslbStatus(gslb *ohmyglbv1beta1.Gslb) error {
-	gslb.Status.ManagedHosts = getGslbManagedHosts(gslb)
-
 	var err error
 
 	gslb.Status.ServiceHealth, err = r.getServiceHealthStatus(gslb)
@@ -31,6 +31,8 @@ func (r *ReconcileGslb) updateGslbStatus(gslb *ohmyglbv1beta1.Gslb) error {
 		return err
 	}
 
+	gslb.Status.GeoTag = os.Getenv("CLUSTER_GEO_TAG")
+
 	err = r.updateHealthyRecordsMetric(gslb, gslb.Status.HealthyRecords)
 	if err != nil {
 		return err
@@ -38,14 +40,6 @@ func (r *ReconcileGslb) updateGslbStatus(gslb *ohmyglbv1beta1.Gslb) error {
 
 	err = r.client.Status().Update(context.TODO(), gslb)
 	return err
-}
-
-func getGslbManagedHosts(gslb *ohmyglbv1beta1.Gslb) []string {
-	var hosts []string
-	for _, rule := range gslb.Spec.Ingress.Rules {
-		hosts = append(hosts, rule.Host)
-	}
-	return hosts
 }
 
 func (r *ReconcileGslb) getServiceHealthStatus(gslb *ohmyglbv1beta1.Gslb) (map[string]string, error) {
@@ -91,26 +85,7 @@ func (r *ReconcileGslb) getServiceHealthStatus(gslb *ohmyglbv1beta1.Gslb) (map[s
 	return serviceHealth, nil
 }
 
-func (r *ReconcileGslb) getHealthyHosts(gslb *ohmyglbv1beta1.Gslb) ([]string, error) {
-	serviceHealth, err := r.getServiceHealthStatus(gslb)
-	if err != nil {
-		return nil, err
-	}
-	var healthyHosts []string
-	for host, status := range serviceHealth {
-		if status == "Healthy" {
-			healthyHosts = append(healthyHosts, host)
-		}
-	}
-
-	return healthyHosts, err
-}
-
 func (r *ReconcileGslb) getHealthyRecords(gslb *ohmyglbv1beta1.Gslb) (map[string][]string, error) {
-	healthyHosts, err := r.getHealthyHosts(gslb)
-	if err != nil {
-		return nil, err
-	}
 
 	dnsEndpoint := &externaldns.DNSEndpoint{}
 
@@ -119,19 +94,19 @@ func (r *ReconcileGslb) getHealthyRecords(gslb *ohmyglbv1beta1.Gslb) (map[string
 		Namespace: gslb.Namespace,
 	}
 
-	err = r.client.Get(context.TODO(), nn, dnsEndpoint)
+	err := r.client.Get(context.TODO(), nn, dnsEndpoint)
 	if err != nil {
 		return nil, err
 	}
 
 	healthyRecords := make(map[string][]string)
 
-	for _, host := range healthyHosts {
-		for _, endpoint := range dnsEndpoint.Spec.Endpoints {
-			if endpoint.DNSName == host {
-				if len(endpoint.Targets) > 0 {
-					healthyRecords[host] = endpoint.Targets
-				}
+	serviceRegex, _ := regexp.Compile("^localtargets")
+	for _, endpoint := range dnsEndpoint.Spec.Endpoints {
+		local := serviceRegex.Match([]byte(endpoint.DNSName))
+		if !local && endpoint.RecordType == "A" {
+			if len(endpoint.Targets) > 0 {
+				healthyRecords[endpoint.DNSName] = endpoint.Targets
 			}
 		}
 	}
