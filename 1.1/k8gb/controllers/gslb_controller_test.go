d74abf861dc7d998ee03a5df5c6dfaaa90a11d53
Yury Tsarev <yury.tsarev@absa.africa>
2020-10-02 21:26:59 +0200

    Fix test memory crash

--- 1.1/k8gb/controllers/gslb_controller_test.go
+++ 1.1/k8gb/controllers/gslb_controller_test.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -78,7 +79,7 @@ func TestGslbController(t *testing.T) {
 	// Create a GslbReconciler object with the scheme and fake client.
 	r := &GslbReconciler{
 		Client: cl,
-		Log:    nil,
+		Log:    ctrl.Log.WithName("setup"),
 		Scheme: s,
 	}
 	r.DepResolver = depresolver.NewDependencyResolver(context.TODO(), r.Client)
