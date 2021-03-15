04bc6b37f09f2c572312d972d31f4169428ce8f3
MichalK <kuritka@gmail.com>
2021-03-15 14:55:32 +0100

    License management with GOLIC (#368)
    - remove /hack folder
     - integrate with golic (Makefile)
     - add .licignore and ignore generated files
     - I had to regenerate all licenses because golic produces additional text
    ```
    Generated by GoLic, for more details see: https://github.com/AbsaOSS/golic
    ```
     - fix license target typo in Makefile

--- hack/insert_headers.sh
+++
@@ -1,9 +0,0 @@
-for f in $(find . -name '*.go'); do
-  if grep -q "Apache License, Version 2.0" $f; then
-      echo "License header is already present. Skipping $f"
-      continue
-  fi
-  echo "Adding header to $f"
-  cat ./hack/boilerplate.go.txt $f > $f.new
-  mv $f.new $f
-done
