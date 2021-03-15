AbsaOSS/ohmyglb

Integration zerolog
In order to integrate a zerolog, I had create log package and take a few steps.

logpackage cntains three components: **factory, singleton and adapter**.

Instead of passing logger into reconciler, providers assistant etc. I created logger [singleton](https://en.wikipedia.org/wiki/Singleton_pattern).
This component is accessible from anywhere and contains one and only one instance of logger which is accessible through reader function `Logger()`
Singleton must be initialised after depresolver reads configuration and internally calls factory which builds logger instance.
Thats why factory is completely private now.

To inject logger into package do following:

```go
import "github.com/AbsaOSS/k8gb/controllers/log"

var logger = log.Logger()

func MyFunc(){
 logger.Info().Msg("hello from logger")
}
```
ControllerManager still calls logr internally. Unfortunately the problem is that the second
logger completely ignores the settings of our logger, so plain text would appear between json records, etc.
It also ignores our formatting. The solution provides [**Adapter** pattern](https://en.wikipedia.org/wiki/Adapter_pattern)
which basically allows us to encapsulate zerolog instance into logr interface. So on the outside it looks like logr but
internally it produces records by our zerolog instance. Here is how we inject adapter:
```go
ctrl.SetLogger(log.NewAdapter(logger))
```

 - I had to remove rlog nad zapr from all components
 - passing logger by arguments was removed, now simpler injection via singleton
 - logging zerolog way:
```go
logger.Info().Msg("hello from zerolog")
logger.Err(err).Msg("error from zerolog")
logger.Info().Msgf("hello from zerolog %s", stringvariable)
logger.Err(err).Msgf("error from zerolog %s", stringvariable)
```
 - We have places where we print Info messages within errors. I replaced them by Warnings
```go
IPs, err := utils.Dig(r.edgeDNSServer, lbHostname)
	if err != nil {
		logger.Warn().Msgf("Can't dig k8gb-coredns-lb servic

controllers/dnsupdate.go
@@ -82,23 +82,23 @@ func (r *GslbReconciler) gslbDNSEndpoint(gslb *k8gbv1beta1.Gslb) (*externaldns.D
 					// If cluster is Primary and Unhealthy return Secondary external targets
 					if health != "Healthy" {
 						finalTargets = externalTargets
-						log.Info(fmt.Sprintf("Executing failover strategy for %s Gslb on Primary. Workload on primary %s cluster is unhealthy, targets are %v",
-							gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets))
+						logger.Info().Msgf("Executing failover strategy for %s Gslb on Primary. Workload on primary %s cluster is unhealthy, targets are %v",
+							gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets)
 					}
 				} else {
 					// If cluster is Secondary and Primary external cluster is Healthy
 					// then return Primary external targets.
 					// Return own targets by default.
 					finalTargets = externalTargets
-					log.Info(fmt.Sprintf("Executing failover strategy for %s Gslb on Secondary. Workload on primary %s cluster is healthy, targets are %v",
-						gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets))
+					logger.Info().Msgf("Executing failover strategy for %s Gslb on Secondary. Workload on primary %s cluster is healthy, targets are %v",
+						gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets)
 				}
 			}
 		} else {
-			log.Info(fmt.Sprintf("No external targets have been found for host %s", host))
+			logger.Info().Msgf("No external targets have been found for host %s", host)
 		}
 
-		log.Info(fmt.Sprintf("Final target list for %s Gslb: %v", gslb.Name, finalTargets))
+		logger.Info().Msgf("Final target list for %s Gslb: %v", gslb.Name, finalTargets)
 
 		if len(finalTargets) > 0 {
 			dnsRecord := &externaldns.Endpoint{

controllers/gslb_controller.go
@@ -20,13 +20,13 @@ import (
 	"context"
 	"fmt"
 
+	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
+	"github.com/AbsaOSS/k8gb/controllers/depresolver"
 	"github.com/AbsaOSS/k8gb/controllers/internal/utils"
+	"github.com/AbsaOSS/k8gb/controllers/log"
 	"github.com/AbsaOSS/k8gb/controllers/providers/dns"
-
 	"github.com/AbsaOSS/k8gb/controllers/providers/metrics"
 
-	"github.com/AbsaOSS/k8gb/controllers/depresolver"
-	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	v1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -36,20 +36,14 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	externaldns "sigs.k8s.io/external-dns/endpoint"
-
-	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
 )
-
-var log = logf.Log.WithName("controller_gslb")
 
 // GslbReconciler reconciles a Gslb object
 type GslbReconciler struct {
 	client.Client
-	Log         logr.Logger
 	Scheme      *runtime.Scheme
 	Config      *depresolver.Config
 	DepResolver *depresolver.DependencyResolver
@@ -65,14 +59,15 @@ const (
 	strategyAnnotation      = "k8gb.io/strategy"
 )
 
+var logger = log.Logger()
+
 // +kubebuilder:rbac:groups=k8gb.absa.oss,resources=gslbs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=k8gb.absa.oss,resources=gslbs/status,verbs=get;update;patch
 
 // Reconcile runs main reconiliation loop
 func (r *GslbReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
-	log := r.Log.WithValues("gslb", req.NamespacedName)
-	result := utils.NewReconcileResultHandler(r.Config.ReconcileRequeueSeconds, log)
+	result := utils.NewReconcileResultHandler(r.Config.ReconcileRequeueSeconds)
 	// Fetch the Gslb instance
 	gslb := &k8gbv1beta1.Gslb{}
 	err := r.Get(ctx, req.NamespacedName, gslb)
@@ -112,6 +107,7 @@ func (r *GslbReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 				return result.RequeueError(err)
 			}
 		}
+		logger.Info().Msg("reconciler exit")
 		return result.Stop()
 	}
 
@@ -176,7 +172,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			c := mgr.GetClient()
 			err := c.List(context.TODO(), gslbList, opts...)
 			if err != nil {
-				log.Info("Can't fetch gslb objects")
+				logger.Info().Msg("Can't fetch gslb objects")
 				return nil
 			}
 			gslbName := ""
@@ -201,8 +197,8 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		})
 
 	createGslbFromIngress := func(annotationKey string, annotationValue string, a handler.MapObject, strategy string) {
-		log.Info(fmt.Sprintf("Detected strategy annotation(%s:%s) on Ingress(%s)",
-			annotationKey, annotationValue, a.Meta.GetName()))
+		logger.Info().Msgf("Detected strategy annotation(%s:%s) on Ingress(%s)",
+			annotationKey, annotationValue, a.Meta.GetName())
 		c := mgr.GetClient()
 		ingressToReuse := &v1beta1.Ingress{}
 		err := c.Get(context.Background(), client.ObjectKey{
@@ -210,7 +206,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			Name:      a.Meta.GetName(),
 		}, ingressToReuse)
 		if err != nil {
-			log.Info(fmt.Sprintf("Ingress(%s) does not exist anymore. Skipping Glsb creation...", a.Meta.GetName()))
+			logger.Info().Msgf("Ingress(%s) does not exist anymore. Skipping Glsb creation...", a.Meta.GetName())
 			return
 		}
 		gslbExist := &k8gbv1beta1.Gslb{}
@@ -219,7 +215,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			Name:      a.Meta.GetName(),
 		}, gslbExist)
 		if err == nil {
-			log.Info(fmt.Sprintf("Gslb(%s) already exists. Skipping Gslb creation...", gslbExist.Name))
+			logger.Info().Msgf("Gslb(%s) already exists. Skipping Gslb creation...", gslbExist.Name)
 			return
 		}
 		gslb := &k8gbv1beta1.Gslb{
@@ -243,15 +239,15 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 				}
 			}
 			if gslb.Spec.Strategy.PrimaryGeoTag == "" {
-				log.Info(fmt.Sprintf("%s annotation is missing, skipping Gslb creation...", primaryGeoTagAnnotation))
+				logger.Info().Msgf("%s annotation is missing, skipping Gslb creation...", primaryGeoTagAnnotation)
 				return
 			}
 		}
 
-		log.Info(fmt.Sprintf("Creating new Gslb(%s) out of Ingress annotation", gslb.Name))
+		logger.Info().Msgf("Creating new Gslb(%s) out of Ingress annotation", gslb.Name)
 		err = c.Create(context.Background(), gslb)
 		if err != nil {
-			log.Error(err, "Glsb creation failed")
+			logger.Err(err).Msg("Glsb creation failed")
 		}
 	}
 	ingressMapFn := handler.ToRequestsFunc(
@@ -280,5 +276,4 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			&handler.EnqueueRequestsFromMapFunc{
 				ToRequests: ingressMapFn}).
 		Complete(r)
-
 }

controllers/ingress.go
@@ -60,19 +60,20 @@ func (r *GslbReconciler) saveIngress(instance *k8gbv1beta1.Gslb, i *v1beta1.Ingr
 	if err != nil && errors.IsNotFound(err) {
 
 		// Create the service
-		log.Info("Creating a new Ingress", "Ingress.Namespace", i.Namespace, "Ingress.Name", i.Name)
+		logger.Info().Msgf("Creating a new Ingress, Ingress.Namespace %s, Ingress.Name: %s", i.Namespace, i.Name)
 		err = r.Create(context.TODO(), i)
 
 		if err != nil {
 			// Creation failed
-			log.Error(err, "Failed to create new Ingress", "Ingress.Namespace", i.Namespace, "Ingress.Name", i.Name)
+			logger.Err(err).Msgf("Failed to create new Ingress Ingress.Namespace: %s, Ingress.Name: %s",
+				i.Namespace, i.Name)
 			return err
 		}
 		// Creation was successful
 		return nil
 	} else if err != nil {
 		// Error that isn't due to the service not existing
-		log.Error(err, "Failed to get Ingress")
+		logger.Err(err).Msg("Failed to get Ingress")
 		return err
 	}
 
@@ -82,13 +83,14 @@ func (r *GslbReconciler) saveIngress(instance *k8gbv1beta1.Gslb, i *v1beta1.Ingr
 		found.Annotations = utils.MergeAnnotations(found.Annotations, i.Annotations)
 		err = r.Update(context.TODO(), found)
 		if errors.IsConflict(err) {
-			r.Log.Info("Ingress has been modified outside of controller, retrying reconciliation",
-				"Ingress.Namespace", found.Namespace, "Ingress.Name", found.Name)
+			logger.Info().Msgf("Ingress has been modified outside of controller, retrying reconciliation"+
+				"Ingress.Namespace %s, Ingress.Name: %s", found.Namespace, found.Name)
 			return nil
 		}
 		if err != nil {
 			// Update failed
-			log.Error(err, "Failed to update Ingress", "Ingress.Namespace", found.Namespace, "Ingress.Name", found.Name)
+			logger.Err(err).Msgf("Failed to update Ingress Ingress.Namespace %s, Ingress.Name: %s",
+				found.Namespace, found.Name)
 			return err
 		}
 	}

controllers/log/log.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controllers
+package log
 
 import (
 	"os"
@@ -25,21 +25,21 @@ import (
 	"github.com/rs/zerolog/pkgerrors"
 )
 
-// LoggerFactory creates new logger
-type LoggerFactory struct {
+// loggerFactory creates new logger
+type loggerFactory struct {
 	log depresolver.Log
 }
 
-func NewLogger(config *depresolver.Config) *LoggerFactory {
+func newLogger(config *depresolver.Config) *loggerFactory {
 	if config == nil {
-		return &LoggerFactory{log: depresolver.Log{}}
+		return &loggerFactory{log: depresolver.Log{}}
 	}
-	return &LoggerFactory{log: config.Log}
+	return &loggerFactory{log: config.Log}
 }
 
 // Get returns new logger even if it doesn't know level or format.
 // In such case returns default logger
-func (l *LoggerFactory) Get() zerolog.Logger {
+func (l *loggerFactory) get() zerolog.Logger {
 	var logger zerolog.Logger
 	var dt = time.RFC822Z
 	if l.log.Format == depresolver.NoFormat {

controllers/log/singleton.go
@@ -1,0 +1,41 @@
+/*
+Copyright 2021 Absa Group Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package log
+
+import (
+	"sync"
+
+	"github.com/AbsaOSS/k8gb/controllers/depresolver"
+	"github.com/rs/zerolog"
+)
+
+var (
+	once sync.Once
+	log  zerolog.Logger
+)
+
+// Logger public static logger, providing instance of initialised logger
+func Logger() *zerolog.Logger {
+	return &log
+}
+
+// Init always initialise logger, no mif config is nil or not
+func Init(c *depresolver.Config) {
+	once.Do(func() {
+		log = newLogger(c).get()
+	})
+}

controllers/providers/dns/external.go
@@ -21,6 +21,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/AbsaOSS/k8gb/controllers/log"
+
 	assistant2 "github.com/AbsaOSS/k8gb/controllers/providers/assistant"
 
 	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
@@ -43,6 +45,8 @@ type ExternalDNSProvider struct {
 	endpointName string
 }
 
+var logger = log.Logger()
+
 func NewExternalDNS(dnsType ExternalDNSType, config depresolver.Config, assistant assistant2.IAssistant) *ExternalDNSProvider {
 	return &ExternalDNSProvider{
 		assistant:    assistant,
@@ -54,7 +58,7 @@ func NewExternalDNS(dnsType ExternalDNSType, config depresolver.Config, assistan
 
 func (p *ExternalDNSProvider) CreateZoneDelegationForExternalDNS(gslb *k8gbv1beta1.Gslb) error {
 	ttl := externaldns.TTL(gslb.Spec.Strategy.DNSTtlSeconds)
-	p.assistant.Info("Creating/Updating DNSEndpoint CRDs for %s...", p)
+	logger.Info().Msgf("Creating/Updating DNSEndpoint CRDs for %s...", p)
 	var NSServerList []string
 	NSServerList = append(NSServerList, nsServerName(p.config))
 	NSServerList = append(NSServerList, nsServerNameExt(p.config)...)

k8gb-io/k8gb

controllers/dnsupdate.go
@@ -83,23 +83,23 @@ func (r *GslbReconciler) gslbDNSEndpoint(gslb *k8gbv1beta1.Gslb) (*externaldns.D
 					// If cluster is Primary and Unhealthy return Secondary external targets
 					if health != "Healthy" {
 						finalTargets = externalTargets
-						log.Info(fmt.Sprintf("Executing failover strategy for %s Gslb on Primary. Workload on primary %s cluster is unhealthy, targets are %v",
-							gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets))
+						log.Info().Msgf("Executing failover strategy for %s Gslb on Primary. Workload on primary %s cluster is unhealthy, targets are %v",
+							gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets)
 					}
 				} else {
 					// If cluster is Secondary and Primary external cluster is Healthy
 					// then return Primary external targets.
 					// Return own targets by default.
 					finalTargets = externalTargets
-					log.Info(fmt.Sprintf("Executing failover strategy for %s Gslb on Secondary. Workload on primary %s cluster is healthy, targets are %v",
-						gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets))
+					log.Info().Msgf("Executing failover strategy for %s Gslb on Secondary. Workload on primary %s cluster is healthy, targets are %v",
+						gslb.Name, gslb.Spec.Strategy.PrimaryGeoTag, finalTargets)
 				}
 			}
 		} else {
-			log.Info(fmt.Sprintf("No external targets have been found for host %s", host))
+			log.Info().Msgf("No external targets have been found for host %s", host)
 		}
 
-		log.Info(fmt.Sprintf("Final target list for %s Gslb: %v", gslb.Name, finalTargets))
+		log.Info().Msgf("Final target list for %s Gslb: %v", gslb.Name, finalTargets)
 
 		if len(finalTargets) > 0 {
 			dnsRecord := &externaldns.Endpoint{

controllers/gslb_controller.go
@@ -20,13 +20,13 @@ import (
 	"context"
 	"fmt"
 
+	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
+	"github.com/AbsaOSS/k8gb/controllers/depresolver"
 	"github.com/AbsaOSS/k8gb/controllers/internal/utils"
+	"github.com/AbsaOSS/k8gb/controllers/logging"
 	"github.com/AbsaOSS/k8gb/controllers/providers/dns"
-
 	"github.com/AbsaOSS/k8gb/controllers/providers/metrics"
 
-	"github.com/AbsaOSS/k8gb/controllers/depresolver"
-	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	v1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -36,20 +36,14 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	externaldns "sigs.k8s.io/external-dns/endpoint"
-
-	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
 )
-
-var log = logf.Log.WithName("controller_gslb")
 
 // GslbReconciler reconciles a Gslb object
 type GslbReconciler struct {
 	client.Client
-	Log         logr.Logger
 	Scheme      *runtime.Scheme
 	Config      *depresolver.Config
 	DepResolver *depresolver.DependencyResolver
@@ -65,14 +59,15 @@ const (
 	strategyAnnotation      = "k8gb.io/strategy"
 )
 
+var log = logging.Logger()
+
 // +kubebuilder:rbac:groups=k8gb.absa.oss,resources=gslbs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=k8gb.absa.oss,resources=gslbs/status,verbs=get;update;patch
 
 // Reconcile runs main reconiliation loop
 func (r *GslbReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
-	log := r.Log.WithValues("gslb", req.NamespacedName)
-	result := utils.NewReconcileResultHandler(r.Config.ReconcileRequeueSeconds, log)
+	result := utils.NewReconcileResultHandler(r.Config.ReconcileRequeueSeconds)
 	// Fetch the Gslb instance
 	gslb := &k8gbv1beta1.Gslb{}
 	err := r.Get(ctx, req.NamespacedName, gslb)
@@ -112,6 +107,7 @@ func (r *GslbReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 				return result.RequeueError(err)
 			}
 		}
+		log.Info().Msg("reconciler exit")
 		return result.Stop()
 	}
 
@@ -176,7 +172,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			c := mgr.GetClient()
 			err := c.List(context.TODO(), gslbList, opts...)
 			if err != nil {
-				log.Info("Can't fetch gslb objects")
+				log.Info().Msg("Can't fetch gslb objects")
 				return nil
 			}
 			gslbName := ""
@@ -201,8 +197,8 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		})
 
 	createGslbFromIngress := func(annotationKey string, annotationValue string, a handler.MapObject, strategy string) {
-		log.Info(fmt.Sprintf("Detected strategy annotation(%s:%s) on Ingress(%s)",
-			annotationKey, annotationValue, a.Meta.GetName()))
+		log.Info().Msgf("Detected strategy annotation(%s:%s) on Ingress(%s)",
+			annotationKey, annotationValue, a.Meta.GetName())
 		c := mgr.GetClient()
 		ingressToReuse := &v1beta1.Ingress{}
 		err := c.Get(context.Background(), client.ObjectKey{
@@ -210,7 +206,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			Name:      a.Meta.GetName(),
 		}, ingressToReuse)
 		if err != nil {
-			log.Info(fmt.Sprintf("Ingress(%s) does not exist anymore. Skipping Glsb creation...", a.Meta.GetName()))
+			log.Info().Msgf("Ingress(%s) does not exist anymore. Skipping Glsb creation...", a.Meta.GetName())
 			return
 		}
 		gslbExist := &k8gbv1beta1.Gslb{}
@@ -219,7 +215,7 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			Name:      a.Meta.GetName(),
 		}, gslbExist)
 		if err == nil {
-			log.Info(fmt.Sprintf("Gslb(%s) already exists. Skipping Gslb creation...", gslbExist.Name))
+			log.Info().Msgf("Gslb(%s) already exists. Skipping Gslb creation...", gslbExist.Name)
 			return
 		}
 		gslb := &k8gbv1beta1.Gslb{
@@ -243,15 +239,15 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 				}
 			}
 			if gslb.Spec.Strategy.PrimaryGeoTag == "" {
-				log.Info(fmt.Sprintf("%s annotation is missing, skipping Gslb creation...", primaryGeoTagAnnotation))
+				log.Info().Msgf("%s annotation is missing, skipping Gslb creation...", primaryGeoTagAnnotation)
 				return
 			}
 		}
 
-		log.Info(fmt.Sprintf("Creating new Gslb(%s) out of Ingress annotation", gslb.Name))
+		log.Info().Msgf("Creating new Gslb(%s) out of Ingress annotation", gslb.Name)
 		err = c.Create(context.Background(), gslb)
 		if err != nil {
-			log.Error(err, "Glsb creation failed")
+			log.Err(err).Msg("Glsb creation failed")
 		}
 	}
 	ingressMapFn := handler.ToRequestsFunc(
@@ -280,5 +276,4 @@ func (r *GslbReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			&handler.EnqueueRequestsFromMapFunc{
 				ToRequests: ingressMapFn}).
 		Complete(r)
-
 }

controllers/ingress.go
@@ -61,19 +61,20 @@ func (r *GslbReconciler) saveIngress(instance *k8gbv1beta1.Gslb, i *v1beta1.Ingr
 	if err != nil && errors.IsNotFound(err) {
 
 		// Create the service
-		log.Info("Creating a new Ingress", "Ingress.Namespace", i.Namespace, "Ingress.Name", i.Name)
+		log.Info().Msgf("Creating a new Ingress, Ingress.Namespace %s, Ingress.Name: %s", i.Namespace, i.Name)
 		err = r.Create(context.TODO(), i)
 
 		if err != nil {
 			// Creation failed
-			log.Error(err, "Failed to create new Ingress", "Ingress.Namespace", i.Namespace, "Ingress.Name", i.Name)
+			log.Err(err).Msgf("Failed to create new Ingress Ingress.Namespace: %s, Ingress.Name: %s",
+				i.Namespace, i.Name)
 			return err
 		}
 		// Creation was successful
 		return nil
 	} else if err != nil {
 		// Error that isn't due to the service not existing
-		log.Error(err, "Failed to get Ingress")
+		log.Err(err).Msg("Failed to get Ingress")
 		return err
 	}
 
@@ -83,13 +84,14 @@ func (r *GslbReconciler) saveIngress(instance *k8gbv1beta1.Gslb, i *v1beta1.Ingr
 		found.Annotations = utils.MergeAnnotations(found.Annotations, i.Annotations)
 		err = r.Update(context.TODO(), found)
 		if errors.IsConflict(err) {
-			r.Log.Info("Ingress has been modified outside of controller, retrying reconciliation",
-				"Ingress.Namespace", found.Namespace, "Ingress.Name", found.Name)
+			log.Info().Msgf("Ingress has been modified outside of controller, retrying reconciliation"+
+				"Ingress.Namespace %s, Ingress.Name: %s", found.Namespace, found.Name)
 			return nil
 		}
 		if err != nil {
 			// Update failed
-			log.Error(err, "Failed to update Ingress", "Ingress.Namespace", found.Namespace, "Ingress.Name", found.Name)
+			log.Err(err).Msgf("Failed to update Ingress Ingress.Namespace %s, Ingress.Name: %s",
+				found.Namespace, found.Name)
 			return err
 		}
 	}

controllers/logging/log.go
@@ -15,7 +15,7 @@ limitations under the License.
 
 Generated by GoLic, for more details see: https://github.com/AbsaOSS/golic
 */
-package controllers
+package logging
 
 import (
 	"os"
@@ -26,21 +26,21 @@ import (
 	"github.com/rs/zerolog/pkgerrors"
 )
 
-// LoggerFactory creates new logger
-type LoggerFactory struct {
+// loggerFactory creates new logger
+type loggerFactory struct {
 	log depresolver.Log
 }
 
-func NewLogger(config *depresolver.Config) *LoggerFactory {
+func newLogger(config *depresolver.Config) *loggerFactory {
 	if config == nil {
-		return &LoggerFactory{log: depresolver.Log{}}
+		return &loggerFactory{log: depresolver.Log{}}
 	}
-	return &LoggerFactory{log: config.Log}
+	return &loggerFactory{log: config.Log}
 }
 
 // Get returns new logger even if it doesn't know level or format.
 // In such case returns default logger
-func (l *LoggerFactory) Get() zerolog.Logger {
+func (l *loggerFactory) get() zerolog.Logger {
 	var logger zerolog.Logger
 	var dt = time.RFC822Z
 	if l.log.Format == depresolver.NoFormat {

controllers/logging/singleton.go
@@ -1,0 +1,42 @@
+/*
+Copyright 2021 Absa Group Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Generated by GoLic, for more details see: https://github.com/AbsaOSS/golic
+*/
+package logging
+
+import (
+	"sync"
+
+	"github.com/AbsaOSS/k8gb/controllers/depresolver"
+	"github.com/rs/zerolog"
+)
+
+var (
+	once sync.Once
+	log  zerolog.Logger
+)
+
+// Logger public static logger, providing instance of initialised logger
+func Logger() *zerolog.Logger {
+	return &log
+}
+
+// Init always initialise logger, no mif config is nil or not
+func Init(c *depresolver.Config) {
+	once.Do(func() {
+		log = newLogger(c).get()
+	})
+}

controllers/providers/dns/external.go
@@ -22,6 +22,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/AbsaOSS/k8gb/controllers/logging"
+
 	assistant2 "github.com/AbsaOSS/k8gb/controllers/providers/assistant"
 
 	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
@@ -44,6 +46,8 @@ type ExternalDNSProvider struct {
 	endpointName string
 }
 
+var log = logging.Logger()
+
 func NewExternalDNS(dnsType ExternalDNSType, config depresolver.Config, assistant assistant2.IAssistant) *ExternalDNSProvider {
 	return &ExternalDNSProvider{
 		assistant:    assistant,
@@ -55,7 +59,7 @@ func NewExternalDNS(dnsType ExternalDNSType, config depresolver.Config, assistan
 
 func (p *ExternalDNSProvider) CreateZoneDelegationForExternalDNS(gslb *k8gbv1beta1.Gslb) error {
 	ttl := externaldns.TTL(gslb.Spec.Strategy.DNSTtlSeconds)
-	p.assistant.Info("Creating/Updating DNSEndpoint CRDs for %s...", p)
+	log.Info().Msgf("Creating/Updating DNSEndpoint CRDs for %s...", p)
 	var NSServerList []string
 	NSServerList = append(NSServerList, nsServerName(p.config))
 	NSServerList = append(NSServerList, nsServerNameExt(p.config)...)