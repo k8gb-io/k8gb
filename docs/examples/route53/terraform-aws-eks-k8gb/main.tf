293f8fc0572d78ba992a6f8af2f609d708bdd5ec
ytsarev <ytsarev@users.noreply.github.com>
2024-09-18 19:33:24 +0000

    Documentation sync from master

--- docs/examples/route53/terraform-aws-eks-k8gb/main.tf
+++ docs/examples/route53/terraform-aws-eks-k8gb/main.tf
@@ -65,7 +65,7 @@ resource "aws_ec2_tag" "eks_elb" {
 module "eks-cluster" {
   tags            = var.eks_tags
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.24.0"
+  version         = "20.24.1"
   cluster_name    = var.cluster_name
   cluster_version = var.kubernetes_version
   subnets         = data.aws_subnet_ids.private.ids
