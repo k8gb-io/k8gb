4c5c7773f2eadd92cd336332b92a4c34243962e3
GitHub Action <action@github.com>
2026-02-19 20:47:27 +0000

    Deployed 30066726 to v0.17.0 with MkDocs 1.6.1 and mike 2.1.3

---
+++ v0.17.0/examples/route53/terraform-aws-eks-k8gb/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+  }
+}
